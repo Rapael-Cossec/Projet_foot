29c1867fab4e7b016859f9e890bd34fe916f3d5d
Raphael Cossec <raphael.cossec@aol.fr>
2019-02-04 18:17:16 +0100

    tournament pour de vrai

---
+++ tournament.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 16:51:52 2019
+
+@author: 3700049
+"""
+
+from Strategy_f.tools import SuperState
+from Strategy_f.Attaquant import StrategyAttaquant
+from Strategy_f.Defenseur import StrategyDefenseur
+from soccersimulator import SoccerTeam
+
+def get_team(nb_players): 
+    team = SoccerTeam(name="Unknown")
+    if nb_players == 1:
+        team.add("Billy",StrategyDefenseur())
+    if nb_players == 2:
+        team.add("Billy", StrategyDefenseur())
+        team.add("Footix", StrategyAttaquant())
+    return team
+
+if __name__ =='__main__':
+    from soccersimulator import Simulation, show_simu
+    team1 = get_team(1)
+    team2 = get_team(2)
+    simu = Simulation (team1,team2)
+    show_simu(simu)
