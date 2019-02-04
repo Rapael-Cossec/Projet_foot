fc072914bef84ca250abaf6d6fd32319444563d2
Raphael Cossec <raphael.cossec@aol.fr>
2019-02-04 18:15:07 +0100

    Tournament, Defense ameliore

---
+++ Strategy_f/Attaquant.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 16:20:32 2019
+
+@author: 3700049
+"""
+GAME_WIDTH = 150
+GAME_HEIGHT = 90
+PLAYER_RADIUS = 1.
+BALL_RADIUS = 0.65
+CAN_SHOOT = PLAYER_RADIUS + BALL_RADIUS
+
+from soccersimulator import Vector2D, SoccerState, SoccerAction
+from soccersimulator import Simulation, SoccerTeam, Player, show_simu
+from soccersimulator import Strategy
+from Strategy_f.tools import SuperState
+from soccersimulator import settings
+import math
+
+class StrategyAttaquant(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Attaquant")
+
+    def compute_strategy(self, state, id_team, id_player):
+        s = SuperState(state, id_team, id_player)
+        # id_team is 1 or 2
+        # id_player starts at 0
+        if(s.dir_ball.norm < CAN_SHOOT):
+            return SoccerAction(s.dir_ball_acc, s.shoot((s.goal_e - s.player)))
+        
+        return SoccerAction(s.dir_ball, Vector2D(0,0))
+"""  
+# Create teams
+team1 = SoccerTeam(name="Team 1")
+team2 = SoccerTeam(name="Team 2")
+
+# Add players
+team2.add("A", StrategyAttaquant())  # Random strategy
+team1.add("B", Strategy())   # Static strategy
+team1.add("C", StrategyFonceur())  # Random strategy
+team2.add("Dr", StrategyDefenseur())   # Static strategy
+
+# Create a match
+simu = Simulation(team1, team2)
+
+# Simulate and display the match
+show_simu(simu)"""
---
+++ Strategy_f/tools.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 28 16:09:02 2019
+
+@author: 3700049
+"""
+GAME_WIDTH = 150
+GAME_HEIGHT = 90
+
+from soccersimulator import Vector2D, SoccerState, SoccerAction
+from soccersimulator import Simulation, SoccerTeam, Player, show_simu
+from soccersimulator import Strategy
+from soccersimulator import settings
+import math
+
+
+
+class SuperState(SoccerState):
+    def __init__(self, state, id_team, id_player):
+        self.state = state
+        self.id_team = id_team
+        self.id_player = id_player
+    
+    def __getattr__(self, attr):
+        return getattr(self.state, attr)
+    
+    @property
+    def ball(self):
+        return self.state.ball.position
+    
+    @property
+    def player(self):
+        return self.state.player_state(self.id_team, self.id_player).position
+    @property
+    def goal_e(self):
+        if (self.id_team==1):
+            return Vector2D(GAME_WIDTH, GAME_HEIGHT/2)
+        return Vector2D(0, GAME_HEIGHT/2)
+    
+    @property
+    def goal_a(self):
+        if (self.id_team==2):
+            return Vector2D(GAME_WIDTH, GAME_HEIGHT/2)
+        return Vector2D(0, GAME_HEIGHT/2)
+    
+    @property
+    def dir_ball(self):
+#       return (self.state.ball.position - self.state.player_state(self.id_team, self.id_player).position).normalize().scale(5)
+        return self.ball - self.player
+   
+    @property
+    def dir_point(self, vec):
+        return self.state.position - vec
+    
+    def distance_balle(self, pointB, distance):
+        return (self.state.ball.position.distance(pointB) < distance)
+    
+    def det_team(nb):
+        if(nb == 1):
+            return 1
+        return 0
+    @property
+    def dir_ball_acc(self):
+        """direction vers balle + acceleration de balle"""
+        return ((self.state.ball.vitesse + self.state.ball.position) * 5 - self.state.player_state(self.id_team, self.id_player).position).normalize().scale(5)
+    
+    
+    def joueur_proche(self, id_team, id_player):
+        opponents = [self.state.player_state(id_team, id_player) for (id_team, id_player) in self.state.players if id_player != self.id_player]
+        return min([(self.player.distance(player.position), player) for player in opponents])[1]
+
+    def joueur_proche_a(self, id_team, id_player):
+        opponents = [self.state.player_state(id_team, id_player).position for (id_team, id_player) in self.state.players if id_team == self.id_team and id_player != self.id_player]
+        return min([(self.player.distance(player.position), player) for player in opponents])[1]
+    
+    def joueur_proche_ball(self, id_team, id_player):
+        opponents = [self.state.player_state(id_team, id_player) for (id_team, id_player) in self.state.players if id_player != self.id_player]
+        return min([(self.ball.distance(player.position), player) for player in opponents])[1]
+
+    
+    def passe(self, Vector):
+        return Vector.normalize().scale(2.5)
+    
+    def shoot(self, Vector):
+        return Vector.normalize().scale(3.8)
+            
