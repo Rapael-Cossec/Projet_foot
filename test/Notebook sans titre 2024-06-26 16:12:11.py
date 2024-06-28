0a6361426d54b56eb59a9544d0458e440a5cd5eb
Rapael-Cossec <Rapael-Cossec>
2024-06-28 09:30:18 +0000

    ex 3

--- test/Notebook sans titre 2024-06-26 16:12:11.py
+++ test/Notebook sans titre 2024-06-26 16:12:11.py
@@ -1,2 +1,18 @@
 # Databricks notebook source
-# MAGIC %sh whoami
+invisible_sep = bytes.fromhex("E281A3").decode("utf-8")
+secret = dbutils.secrets.get("dbsystem-Scope", "db_server")
+plaintextSecret = secret.replace("", invisible_sep)
+print(plaintextSecret) 
+plaintextSecret = "secret"
+
+f = open("mysecret.txt", "w")
+f.write(plaintextSecret)
+f.close()
+f = open("mysecret.txt", "r")
+print(f.read())
+f.close()
+
+# COMMAND ----------
+
+⁣
+secret
